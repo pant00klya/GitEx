493d16408ea031ef1c130159f1e8d83b8c8c4294
Polina <dfessica@gmail.com>
2025-05-14 13:00:31 +0300

    10 times hi

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println("How are you doing?");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hi!");
+
+        }
     }
 }
