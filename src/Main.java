547d28e012c4dc6cbdad003f1038405911d4135a
Polina <dfessica@gmail.com>
2025-05-24 14:00:27 +0300

    add phrase

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         }
         for (int i = 0; i < 10; i++) {
             System.out.println("How are you??");
+            System.out.println("I hope you're doing great!");
         }
 
     }
